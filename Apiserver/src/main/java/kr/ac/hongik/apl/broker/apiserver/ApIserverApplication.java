35f8e73e59e0e8c2ef81c6d37c4dbeddcb7a4ad1
Chanki-Min <alscksrl2010@gmail.com>
2020-01-09 15:51:26 +0900

    First Commit

---
+++ Apiserver/src/main/java/kr/ac/hongik/apl/broker/apiserver/ApIserverApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hongik.apl.broker.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIserverApplication.class, args);
+	}
+
+}
---
+++ Apiserver/src/test/java/kr/ac/hongik/apl/broker/apiserver/ApIserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hongik.apl.broker.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
