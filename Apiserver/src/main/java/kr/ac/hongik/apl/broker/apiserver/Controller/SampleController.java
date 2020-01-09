a11dd6fafcdac08ea5af5765690c286115bbdfd4
Chanki-Min <alscksrl2010@gmail.com>
2020-01-09 21:13:25 +0900

    Make dev environment ,make APIserver exclude PBFT's log4j dependency, and make sampleController
    개발 환경 구축 완료, APIserver와 PBFT의 log4j 디펜던시 충돌 문제를 해결함

---
+++ Apiserver/src/main/java/kr/ac/hongik/apl/broker/apiserver/Controller/SampleController.java
@@ -0,0 +1,40 @@
+package kr.ac.hongik.apl.broker.apiserver.Controller;
+
+import kr.ac.hongik.apl.Client;
+import kr.ac.hongik.apl.ES.EsJsonParser;
+import kr.ac.hongik.apl.Messages.RequestMessage;
+import kr.ac.hongik.apl.Operations.Operation;
+import kr.ac.hongik.apl.Replica;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Slf4j
+@Controller
+public class SampleController {
+
+	@RequestMapping(value = "/test")
+	@ResponseBody
+	public String test() throws IOException {
+		InputStream in = getClass().getResourceAsStream("/replica.properties");
+		Properties prop = new Properties();
+		prop.load(in);
+
+		Client client = new Client(prop);
+
+		Operation op = new kr.ac.hongik.apl.Operations.Dev.GreetingOperation(client.getPublicKey());
+		RequestMessage requestMessage = RequestMessage.makeRequestMsg(client.getPrivateKey(), op);
+		client.request(requestMessage);
+
+		kr.ac.hongik.apl.Operations.Dev.Greeting reply = (kr.ac.hongik.apl.Operations.Dev.Greeting) client.getReply();
+		client.close();
+
+		return reply.greeting;
+	}
+}
